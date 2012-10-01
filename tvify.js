5c812633954fbcb214b0d664f4716f820320728d
Christiaan Baartse <anotherhero@gmail.com>
2012-10-01 23:06:13 +0200

    When loading Gmail the initial screen could be zoomed 300% While when testing after a few seconds it should be much less.

--- tvify.js
+++ tvify.js
@@ -1,26 +1,19 @@
 (function() {
-	var zoomBody = function() {
-		var body = document.body,
-			zoom = 100,
-			increment = 25,
-			max = 300,
-			initialWidth = body.scrollWidth;
-			
-		if (body.style && body.style.zoom) {
-			zoom = body.style.zoom;
-		}
-
-
-		while (zoom < max && initialWidth == body.scrollWidth) {
-			// console.log(body.scrollWidth, body.clientWidth, zoom);
-			zoom += increment;
-			body.style.zoom = zoom+'%';
-		}
-		// console.log(body.scrollWidth, body.clientWidth, zoom);
-		body.style.zoom = (zoom - increment) + '%';
-	};
-	
-	zoomBody();
-	setTimeout(zoomBody, 3000);
+    var zoomBody = function() {
+        var body = document.body,
+            zoom = 100,
+            increment = 25,
+            max = 300,
+            initialWidth = body.scrollWidth;
+            
+        while (zoom < max && initialWidth == body.scrollWidth) {
+            zoom += increment;
+            body.style.zoom = zoom+'%';
+        }
+        body.style.zoom = (zoom - increment) + '%';
+    };
+    
+    zoomBody();
+    setInterval(zoomBody, 3000);
 })();
 
