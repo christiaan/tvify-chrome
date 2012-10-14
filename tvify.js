40e3375080800c6b62331d13d3907f2cc96cfd5d
Christiaan Baartse <anotherhero@gmail.com>
2012-10-14 19:55:04 +0200

    Button to toggle between zooming a page and not

--- tvify.js
+++ tvify.js
@@ -1,10 +1,23 @@
 (function() {
+    var body = document.body,
+        initialWidth = body.scrollWidth,
+        disabled = false;
+
+    chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.toggle) {
+            disabled = !disabled;
+            zoomBody();
+        }
+    });
+
     var zoomBody = function() {
-        var body = document.body,
-            zoom = 100,
+        if (disabled) {
+            body.style.zoom = '100%';
+            return;
+        }
+        var zoom = 100,
             increment = 25,
-            max = 300,
-            initialWidth = body.scrollWidth;
+            max = 300;
             
         while (zoom < max && initialWidth == body.scrollWidth) {
             zoom += increment;
